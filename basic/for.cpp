f48c01a3bc87e3c4d1f6903277d68c9df2e3f901
bingohuang <me@bingohuang.com>
2020-02-10 16:10:06 +0800

    add for

---
+++ basic/for.cpp
@@ -0,0 +1,17 @@
+//
+// Created by BingoHuang on 2020/2/9.
+//
+
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+
+    for( ; ; )
+    {
+        printf("This loop will run forever.\n");
+    }
+
+    return 0;
+}
